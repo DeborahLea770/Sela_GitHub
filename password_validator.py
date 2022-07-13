39883e930271d78a596d0ebde5812dfb23ba342b
deborahlea770 <deborahlea770@gmail.com>
2022-07-13 22:32:20 +0300

    first commit

---
+++ password_validator.py
@@ -0,0 +1,38 @@
+import sys
+from colorama import Fore
+import argparse
+
+def password_check(passwd):
+    val = 0
+
+    if len(passwd) < 10:
+        print('length should be at least 10')
+        val = 1
+
+    if not any(char.isdigit() for char in passwd):
+        print('Password should have at least one numeral')
+        val = 1
+
+    if not any(char.isupper() for char in passwd):
+        print('Password should have at least one uppercase letter')
+        val = 1
+
+    if not any(char.islower() for char in passwd):
+        print(f'{Fore.WHITE}Password should have at least one lowercase letter')
+        val = 1
+
+    return val
+
+# Main method
+def main():
+    passwd = sys.argv[1]
+
+    if password_check(passwd) == 0:
+        print(f"{Fore.GREEN}Password is valid")
+    else:
+        print(f"{Fore.RED}Invalid Password !!")
+    print(Fore.WHITE)
+
+# Driver Code
+if __name__ == '__main__':
+    main()
